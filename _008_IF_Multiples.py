5c81f061d6643b41eb2012b1ce1f912052cd9574
victoralema <161276656+victoralema@users.noreply.github.com>
2024-03-03 22:16:43 -0600

    Add files via upload

---
+++ _008_IF_Multiples.py
@@ -0,0 +1,12 @@
+##21310193 Victor Eduardo Aleman Padilla
+##Multiples if
+entrada = int(input('Introduce un número del 1 al 4:\n'))##Creamos varibale de entrada e imprimimos instruccion
+
+if entrada == 1:##Evaluamos entrada y si es verdadera ejecuta if
+    print('Has seleccionado la opción número 1.')##Imprimimos Texto con la opcion seleccionada
+if entrada == 2:##Evaluamos entrada y si es verdadera ejecuta if
+    print('Has seleccionado la opción número 2.')##Imprimimos Texto con la opcion seleccionada
+if entrada == 3:##Evaluamos entrada y si es verdadera ejecuta if
+    print('Has seleccionado la opción número 3.')##Imprimimos Texto con la opcion seleccionada
+if entrada == 4:##Evaluamos entrada y si es verdadera ejecuta if
+    print('Has seleccionado la opción número 4.')##Imprimimos Texto con la opcion seleccionada
