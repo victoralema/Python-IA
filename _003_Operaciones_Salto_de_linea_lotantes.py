be64b8e9cf792241af6b6db26a159fcad901cda2
victoralema <161276656+victoralema@users.noreply.github.com>
2024-03-03 21:35:19 -0600

    Add files via upload

---
+++ _003_Operaciones_Salto_de_linea_lotantes.py
@@ -0,0 +1,12 @@
+##21310193 Victor Eduardo Aleman Padilla
+##Uso de saltos de linea y todas las operaciones
+numero1 = 2##Creamos variable y guardamos un dato
+numero2 = 3##Creamos variable y guardamos un dato
+print("Se haran las siguientes operaciones con los siguientes numeros ",numero1," y ",numero2,"\n")##imrpimimos instruccion
+print("Suma = ",numero1+numero2,"\n")##Sumamos el primer numero con el segundo e imprimimos
+print("Resta = ",numero1-numero2,"\n")##Restamos el primer numero con el segundo e imprimimos
+print("Multiplicacion = ",numero1*numero2,"\n")##Multiplicamos el primero numero por el segundo e imprimimos
+division = numero1/numero2##Dividimos el primer numero con el segundo
+print("Division con todos los decimales = ",division,"\n")##imprimimos el valor completo de la division
+print("Division con 2 decimales = ",round(division,2),"\n")##imprimimos solo 2 decimales del resultado de la division
+print("Exponencial = ",numero1*numero1*numero1*numero1,"\n")##multiplicamos el numero1 el numero de veces de numero 2
