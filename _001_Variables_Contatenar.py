b37431cad51224f4596ac8c80ad64bb428f4c46b
victoralema <161276656+victoralema@users.noreply.github.com>
2024-03-03 21:34:45 -0600

    Add files via upload

---
+++ _001_Variables_Contatenar.py
@@ -0,0 +1,5 @@
+##21310193 Victor Eduardo Aleman Padilla
+mensaje="Hola, "##creamos una variable donde guardaremos un dato
+mensaje2 = "como estas?"##creamos otra variable donde guardaremos nuestro segundo dato
+resultado = mensaje + mensaje2##sumamos nuestros dos variables de mensaje
+print(resultado)##imprimimos nuestro dato guardado en resultado
