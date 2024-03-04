44b9dac01c52c336da44ac2fb9fbc5115df47c36
victoralema <161276656+victoralema@users.noreply.github.com>
2024-03-03 22:06:47 -0600

    Add files via upload

---
+++ _005_Tupla_Crear_y_manejar.py
@@ -0,0 +1,5 @@
+##21310193 Victor Eduardo Aleman Padilla
+##Se usaran las funciones tupla crear, manejar y convertir listas a tupla
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']##Creamos una lista con varios elementos
+colores_tupla = tuple(colores)##la convertimos en tupla
+print(type(colores_tupla))##convertimos a tupla nuestra lista
