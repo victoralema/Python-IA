8ba0db2360343350b1ba99a69201f8c6806ac201
victoralema <161276656+victoralema@users.noreply.github.com>
2024-03-03 21:34:59 -0600

    Add files via upload

---
+++ _002_Metodos_upper-lower-title.py
@@ -0,0 +1,8 @@
+##21310193 Victor Eduardo Aleman Padilla
+##funcion title, lower, upper
+nombre = "programación fácil".title()##imprimir texto con la primera letra mayuscula
+print(nombre)##imprimir variable
+nombre = "ProGRAmacIOn FáCil".lower()##imprimir texto en minusculas
+print(nombre)##imprimir variable
+nombre = "ProGRAmacIOn FáCil".upper()##imprimir texto en mayusculas
+print(nombre)##imprimir variable
